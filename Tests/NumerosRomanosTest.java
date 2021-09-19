ea97a883bdaab42c45b83d9024025ef937dcbb57
Davidama <davidarteromartinez@gmail.com>
2021-09-19 22:54:35 +0200

    Verification tests numbers >30

--- Tests/NumerosRomanosTest.java
+++ Tests/NumerosRomanosTest.java
@@ -1,8 +1,14 @@
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.junit.Assert.assertEquals;
 
 public class NumerosRomanosTest {
 
+    NumerosRomanos numerosRomanos = new NumerosRomanos();
     @Test
     public void pasarAromanos(){
         NumerosRomanos numerosRomanos = new NumerosRomanos();
@@ -65,6 +71,71 @@ public class NumerosRomanosTest {
         NumerosRomanos numerosRomanos = new NumerosRomanos();
         String romano = numerosRomanos.convertirAromanos(9);
         assertEquals(romano, "IX");
+    }
+
+    @Test
+    public void pasar14Aromanos(){
+        NumerosRomanos numerosRomanos = new NumerosRomanos();
+        String romano = numerosRomanos.convertirAromanos(14);
+        assertEquals(romano, "XIV");
+    }
+
+    @Test
+    public void verificarVeintenaRomanos(){
+        List<Integer> numerosNaturales = Arrays.asList(20,21,22,23,24,25,26,27,28,29);
+
+        List<String> romanos = numerosNaturales.stream()
+                .map(number-> numerosRomanos.convertirAromanos(number))
+                .collect(Collectors.toList());
+
+        assertEquals(romanos.get(0), "XX");
+        assertEquals(romanos.get(1), "XXI");
+        assertEquals(romanos.get(2), "XXII");
+        assertEquals(romanos.get(3), "XXIII");
+        assertEquals(romanos.get(4), "XXIV");
+        assertEquals(romanos.get(5), "XXV");
+        assertEquals(romanos.get(6), "XXVI");
+        assertEquals(romanos.get(7), "XXVII");
+        assertEquals(romanos.get(8), "XXVIII");
+        assertEquals(romanos.get(9), "XXIX");
+    }
+
+    @Test
+    public void verificarTreintenaRomanos(){
+        List<Integer> numerosNaturales = Arrays.asList(30,31,32,33,34,35,36,37,38,39);
+
+        List<String> romanos = numerosNaturales.stream()
+                .map(number-> numerosRomanos.convertirAromanos(number))
+                .collect(Collectors.toList());
+
+        assertEquals(romanos.get(0), "XXX");
+        assertEquals(romanos.get(1), "XXXI");
+        assertEquals(romanos.get(2), "XXXII");
+        assertEquals(romanos.get(3), "XXXIII");
+        assertEquals(romanos.get(4), "XXXIV");
+        assertEquals(romanos.get(5), "XXXV");
+        assertEquals(romanos.get(6), "XXXVI");
+        assertEquals(romanos.get(7), "XXXVII");
+        assertEquals(romanos.get(8), "XXXVIII");
+        assertEquals(romanos.get(9), "XXXIX");
+    }
+
+    @Test
+    public void verificarDecimalesRomanos(){
+        List<Integer> numerosNaturales = Arrays.asList(40,50,60,70,80,90,88,67);
+
+        List<String> romanos = numerosNaturales.stream()
+                .map(number-> numerosRomanos.convertirAromanos(number))
+                .collect(Collectors.toList());
+
+        assertEquals(romanos.get(0), "XL");
+        assertEquals(romanos.get(1), "L");
+        assertEquals(romanos.get(2), "LX");
+        assertEquals(romanos.get(3), "LXX");
+        assertEquals(romanos.get(4), "LXXX");
+        assertEquals(romanos.get(5), "XC");
+        assertEquals(romanos.get(6), "LXXXVIII");
+        assertEquals(romanos.get(7), "LXVII");
     }
 
 
--- src/NumerosRomanos.java
+++ src/NumerosRomanos.java
@@ -1,12 +1,21 @@
-public class NumerosRomanos {
-    public String convertirAromanos(int numeroNatural) {
+import com.sun.org.apache.bcel.internal.generic.RETURN;
 
-        switch (numeroNatural) {
-            case 4:
-                return "IV";
-            case 9:
-                return "IX";
+public class NumerosRomanos {
+    public String convertirAromanos(Integer numeroNatural) {
+
+        char[] numerosChart = numeroNatural.toString().toCharArray();
+
+        if(numerosChart.length>=2){
+            String unidadRomana = pasarUnidad(Character.getNumericValue(numerosChart[1]));
+            String decenaRomana = pasarDecena(Character.getNumericValue(numerosChart[0]));
+
+            return decenaRomana + unidadRomana;
+
         }
+        if(numerosChart.length>=1){
+            return pasarUnidad(Character.getNumericValue(numerosChart[0]));
+        }
+
 
 
         if(numeroNatural<=3){
@@ -21,10 +30,58 @@ public class NumerosRomanos {
     }
 
 
+    private String pasarDecena(int decena) {
+
+        switch (decena) {
+            case 4:
+                return "XL";
+            case 9:
+                return "XC";
+        }
+
+        if(decena<=3){
+            return sumarX(1,decena, "");
+        }
+
+        if(decena<=8){
+            return sumarX(6, decena, "L");
+        }
+        return null;
+
+    }
+
+    private String pasarUnidad(int unidad) {
+        switch (unidad) {
+            case 4:
+                return "IV";
+            case 9:
+                return "IX";
+        }
+
+        if(unidad<=3){
+            return sumarI(1,unidad, "");
+        }
+
+        if(unidad<=8){
+            return sumarI(6, unidad, "V");
+        }
+        return null;
+    }
+
+
     private String sumarI(int inicioCuenta, int numeroNatural, String inicioNumeroRomano){
         for (int i = inicioCuenta; i <= numeroNatural; i++) {
             inicioNumeroRomano +="I";
         }
         return inicioNumeroRomano;
     }
+
+    private String sumarX(int inicioCuenta, int numeroNatural, String inicioNumeroRomano){
+        for (int i = inicioCuenta; i <= numeroNatural; i++) {
+            inicioNumeroRomano +="X";
+        }
+        return inicioNumeroRomano;
+    }
+
+
 }
